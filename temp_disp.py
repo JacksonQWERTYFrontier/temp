2f0273d7c839aecd3d13c7176edabf4819920ec9
JacksonQWERTYFrontier <2025remy.fritz@fcboe.org>
2025-01-14 13:34:30 -0500

    pht_sensor.py.py

--- temp_disp.py
+++ temp_disp.py
@@ -1,43 +1,38 @@
-"""
-This test will initialize the display using displayio and draw a solid green
-background, a smaller purple rectangle, and some yellow text.
-"""
+import time
 import board
 import terminalio
 import displayio
-import time
-import pwmio
 from analogio import AnalogIn
-from digitalio import DigitalInOut, Direction
-# Starting in CircuitPython 9.x fourwire will be a seperate internal library
-# rather than a component of the displayio library
-try:
-    from fourwire import FourWire
-except ImportError:
-    from displayio import FourWire
+from adafruit_ms8607 import MS8607
 from adafruit_display_text import label
+from fourwire import FourWire
 from adafruit_st7789 import ST7789
 
-# First set some parameters used for shapes and text
+# Set some parameters used for shapes and text
 BORDER = 20
 FONTSCALE = 2
-BACKGROUND_COLOR = 0x00FF00  # Bright Green
-FOREGROUND_COLOR = 0xAA0088  # Purple
-TEXT_COLOR = 0xFFFF00
+BACKGROUND_COLOR = 0xAA0000  # Black
+FOREGROUND_COLOR = 0xAA0000  # Purple
+TEXT_COLOR = 0xFFFFFF
 
-# Release any resources currently in use for the displays
+# Release any resources currently being used
 displayio.release_displays()
 
+# Initialize I2C and the MS8607 sensor
+i2c = board.I2C()  # Initialize I2C bus
+sensor = MS8607(i2c)  # Create MS8607 sensor object
+
+# Set up SPI 
+temp = AnalogIn(board.A0)
 spi = board.SPI()
 tft_cs = board.D2
 tft_dc = board.D3
-
 display_bus = FourWire(spi, command=tft_dc, chip_select=tft_cs)
 display = ST7789(
     display_bus, rotation=270, width=240, height=135, rowstart=40, colstart=53
 )
 
-# Make the display context
+# display context
 splash = displayio.Group()
 display.root_group = splash
 
@@ -48,7 +43,7 @@ color_palette[0] = BACKGROUND_COLOR
 bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
 splash.append(bg_sprite)
 
-# Draw a smaller inner rectangle
+# This draws a smaller rectangle
 inner_bitmap = displayio.Bitmap(
     display.width - BORDER * 2, display.height - BORDER * 2, 1
 )
@@ -59,31 +54,28 @@ inner_sprite = displayio.TileGrid(
 )
 splash.append(inner_sprite)
 
-# Draw a label
-
-pot = AnalogIn(board.A0)
-
-updating_label = label.Label(
-font=terminalio.FONT, text=str ("hi"), scale=2)
-updating_label.anchor_point = (0, 0)
-updating_label.anchored_position = (20, 20)
-splash.append(updating_label)
-
-
-MAXpot = 65535
-x = 0
-v = 65535
-p = 0
-
+# This is the main Counter for cycling through Pressure, Humidity, and Temperature
+counter = 0
+maxxed = 65535
+vlt = 0
+# This is the main loop to display sensor data
 while True:
-
-    a = pot.value
-    b = (a /MAXpot) * 3.3
-    c = (b - 0.5) * 100
-    d = (c*(1.8) + 32.0)
-    e = round(d,2)
-    print(e)
-    text = str (e) + " Deg F"
+    hot = temp.value / maxxed * 3.3
+    temp_C = (hot - .5) *100
+    temp_F = (9/5) * temp_C +32
+    hot = round(temp_F,2)
+    print(temp_F)
+    pressure = sensor.pressure  
+    humidity = sensor.relative_humidity
+    if counter == 0:
+        text = "Humidity: %.2f %% " % humidity
+    elif counter == 1:
+        text = "Pressure: %.2f hPa" % pressure
+    elif counter == 2:
+        text = str (hot) + " F"
+    counter += 1
+    if counter >= 3:
+        counter = 0  
     text_area = label.Label(terminalio.FONT, text=text, color=TEXT_COLOR)
     text_width = text_area.bounding_box[2] * FONTSCALE
     text_group = displayio.Group(
@@ -91,10 +83,7 @@ while True:
         x=display.width // 2 - text_width // 2,
         y=display.height // 2,
     )
-    text_group.append(text_area)  # Subgroup for text scaling
+    text_group.append(text_area)  
     splash.append(text_group)
-
-    # add label to group that is showing on display
-    updating_label.text = str (e)
-    time.sleep(5)
+    time.sleep(3)
     splash.remove(text_group)
